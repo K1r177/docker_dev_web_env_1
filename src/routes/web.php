de36f87ce5abb6228f3a9a87280c260e27674f84
K1r177 <K1r177@yandex.ru>
2024-10-14 07:04:52 +0300

    lesson-2

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    //dump("Hello");
+    //dd("Hello");
+    //dump(config('app.locale'));
+    //dump(config('database.connections.mysql'));
+    //dump(config('app.env'));
+    dump(config('custom.custom_test'));
     return view('welcome');
 });
