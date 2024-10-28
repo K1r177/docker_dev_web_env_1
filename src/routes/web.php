2b7a9579f1f0f8bbead41341e285b5eff1585908
K1r177 <K1r177@yandex.ru>
2024-10-28 23:58:01 +0300

    урок - 4

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
+}); */
+
+/* Route::get('/', function(){
+    return '<h1>Hello world</h1>';
+}); */
+
+/* Route::get('/', function(){
+    return view('hi', ['title' => 'Main page']);
+}); */
+
+/* Route::view('/', 'test.hi', ['title' => 'Main page']);
+
+Route::get('/posts/{id?}', function($id = 1){
+    return "Post ID {$id}";
 });
+
+Route::get('/posts/{id?}/comments/{comment?}', function($id = 1, $comment_id){
+    return "Post ID {$id}, Comment ID {$comment_id}";
+}); */
+
+Route::get('/posts/{id}', function($id){
+    return "Post ID {$id}";
+});
+
+Route::get('/posts/contact', function(){
+    return "Post SLUG CONTACT";
+});
+
+Route::get('/posts/{slug}', function($slug){
+    return "Post SLUG {$slug}";
+});
+
+Route::get('/search/{search}', function($search){
+    return "Searching: {$search}";
+})->where(['search' => '.*']);
+
+Route::post('posts', function(){
+    return 'Store post';
+})->withoutMiddleware(VerifyCsrfToken::class);
+
+/* Route::match(['get', 'post'], '/get-post', function(){
+    return 'Hello from GET|POST'; 
+})->withoutMiddleware(VerifyCsrfToken::class); */
+
+Route::any('/get-post', function(){
+    return 'Hello from ANY METHOD'; 
+})->withoutMiddleware(VerifyCsrfToken::class);
+
+Route::redirect('/here', '/get-post', 301);
